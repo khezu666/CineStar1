c88e26f8e2b6d2d45fcb5342f93fc3e8ba1ef56a
khezu666 <diegoaxelflores2301@gmail.com>
2024-07-09 23:56:12 -0400

    pan

--- assets/js/Modo-claro.js
+++ assets/js/Modo-claro.js
@@ -7,5 +7,20 @@ toggle.addEventListener('change',(event)=>{
         iconito.innerHTML='<i class="fa-solid fa-moon"></i>';
     }else{
         iconito.innerHTML='<i class="fa-solid fa-sun"></i>';
+    };
+
+    // guardamos el modo en localstorage.
+    if(document.body.classList.contains('light')){
+        localStorage.setItem('light-mode', 'true');
+    }else {
+        localStorage.setItem('light-mode', 'false');
     }
-})
+
+});
+
+// obtenemos el modo actual.
+if(localStorage.getItem('light-mode') === 'true'){
+    document.body.classList.add('light');
+} else {
+    document.body.classList.remove('light');
+}
