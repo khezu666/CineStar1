c5e729e345cd81eac79044024aa4cfca6be995b1
AAndersJ12 <anderabreu1@gmail.com>
2024-07-12 23:59:04 -0400

    inicio crud arreglado icono

--- assets/js/Modo-claro.js
+++ assets/js/Modo-claro.js
@@ -1,26 +1,24 @@
-let toggle=document.getElementById('toggle');
-let iconito=document.getElementById('iconito');
-toggle.addEventListener('change',(event)=>{
-    let checked=event.target.checked
+let toggle = document.getElementById('toggle');
+let iconito = document.getElementById('iconito');
+
+toggle.addEventListener('change', (event) => {
+    let checked = event.target.checked;
     document.body.classList.toggle('light');
-    if(checked==true){
-        iconito.innerHTML='<i class="fa-solid fa-moon"></i>';
-    }else{
-        iconito.innerHTML='<i class="fa-solid fa-sun"></i>';
+    if (checked) {
+        iconito.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    } else {
+        iconito.innerHTML = '<i class="fa-solid fa-sun"></i>';
     };
 
     // guardamos el modo en localstorage.
-    if(document.body.classList.contains('light')){
-        localStorage.setItem('light-mode', 'true');
-    }else {
-        localStorage.setItem('light-mode', 'false');
-    }   
+    localStorage.setItem('light-mode', checked);
 });
 
 // obtenemos el modo actual.
-if(localStorage.getItem('light-mode') === 'true'){
+if (localStorage.getItem('light-mode') === 'true') {
     document.body.classList.add('light');
+    iconito.innerHTML = '<i class="fa-solid fa-moon"></i>';
 } else {
     document.body.classList.remove('light');
+    iconito.innerHTML = '<i class="fa-solid fa-sun"></i>';
 }
-
--- assets/js/crudUsuario.js
+++ assets/js/crudUsuario.js
@@ -83,15 +83,70 @@ function renderTable() {
 
 renderTable();
 */
-const listUsers = async() => {
-    try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        const data=response.json();
-    }catch{ex}{
-        alert(ex)
+let dataTable;
+let dataTableIsInitialized=false
+
+const dataTableOptions={
+    lengthMenu:[5,10,15,20],
+    columnDefs:[
+        {className:"centered",targets:[0,1,2,3,4,5,6]},
+        {orderable:false, targets:[5,6]},
+        {width: "5%", targets:[0]}
+    ],
+    pageLength:3,
+    destroy:true,
+    language: {
+        lengthMenu: "Mostrar _MENU_ registros por página",
+        zeroRecords: "No se encontraron resultados",
+        info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registro",
+        infoEmpty: "Ninguún usuario encontrado",
+        infoFiltered: "(filtrado de un total de _MAX_ registros)",
+        search: "Buscar:",
+        loadingRecords: "Cargando...",
+        paginate: {
+            first: "Primero",
+            last: "Último",
+            next: "Siguiente",
+            previous: "Anterior"
+        }
     }
 }
 
-window.addEventListener("load", async() => {
-await listUsers();
+const initDatatable = async() =>{
+    if(dataTableIsInitialized){
+        dataTable.destroy
+    }
+    await listUsers();
+    dataTable=$("#datatable-users").DataTable(dataTableOptions);
+    dataTableIsInitialized=true;
+};
+
+const listUsers = async () => {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await response.json();
+
+        let content = ``;
+        users.forEach((user, index) => {
+            content+=`
+            <tr>
+                <td>${index + 1}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.address.city}</td>
+                <td>${user.company.name}</td>
+                <td><i class="fa-solid fa-check"></i></td>
+                <td><button class="btn btn-sm btn-primary">
+                <i class="fa-solid fa-pencil"></i></button><button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash"></i></button></td>
+            </tr>`;
+        });
+        const usuarios = document.querySelector('#tableBody_users')
+        usuarios.innerHTML = content;
+    }catch(ex){
+        alert(ex);
+    }
+};
+
+window.addEventListener("load", async () => {
+await initDatatable();  
 });
